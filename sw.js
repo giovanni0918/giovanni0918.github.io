729aaa244308c3fedff11b742b3b2e09a8e846dc
Giovanni Orlando <gorlandor@gmail.com>
2016-08-23 19:57:38 -0400

    Enforce gitignore, Update SW
    Added to giovanni0918.github.io v0.10:
    cache /pages directory
    remove node_modules' binaries from repo
    
    setup custom domain:
    https://help.github.com/articles/using-a-custom-domain-with-github-pages
    /
    setup tls: https

--- sw.js
+++ sw.js
@@ -2,10 +2,16 @@ importScripts('node_modules/sw-toolbox/sw-toolbox.js');
 toolbox.options.debug = false;
 
 toolbox.precache([
-	'/',
-	'/css/styles.css',
-	'/js/app.js'
+    '/',
+    '/css/styles.min.css',
+    '/js/app.js',
+    '/pages/blog/',
+    '/pages/contact/',
+    '/pages/twitter/'
 ]);
 toolbox.router.get('/', toolbox.fastest);
-toolbox.router.get('/css/styles.css', toolbox.fastest);
+toolbox.router.get('/css/styles.min.css', toolbox.fastest);
 toolbox.router.get('/js/app.js', toolbox.fastest);
+toolbox.router.get('/pages/blog/', toolbox.fastest);
+toolbox.router.get('/pages/contact/', toolbox.fastest);
+toolbox.router.get('/pages/twitter/', toolbox.fastest);
