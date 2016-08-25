4190fd94672d17f4c704c8598c5a07834ca60603
Giovanni Orlando <gorlandor@gmail.com>
2016-08-25 09:16:52 -0400

    Update SW
    Forgot to update service-worker to use the bundled css.

--- sw.js
+++ sw.js
@@ -3,14 +3,14 @@ toolbox.options.debug = false;
 
 toolbox.precache([
     '/',
-    '/css/styles.min.css',
+    '/dist/styles.css',
     '/js/app.js',
     '/pages/blog/',
     '/pages/contact/',
     '/pages/twitter/'
 ]);
 toolbox.router.get('/', toolbox.fastest);
-toolbox.router.get('/css/styles.min.css', toolbox.fastest);
+toolbox.router.get('/dist/styles.css', toolbox.fastest);
 toolbox.router.get('/js/app.js', toolbox.fastest);
 toolbox.router.get('/pages/blog/', toolbox.fastest);
 toolbox.router.get('/pages/contact/', toolbox.fastest);
