d31c8eec47827c51910382d4a9436ca35f6b7b94
Giovanni Orlando <gorlandor@gmail.com>
2016-08-26 03:45:05 -0400

    Fix SW import, Update styles
    Added to giovanni0918.github.io v0.12:
    Bundled sw-toolbox,
    Cached dist/images,
    Update gulpfile,
    index media queries for laptop and desktops.
    
    setup custom domain:
    https://help.github.com/articles/using-a-custom-domain-with-github-pages
    /
    setup tls: https

--- gulpfile.js
+++ gulpfile.js
@@ -11,27 +11,32 @@ gulp.task('imagemin', () =>
     gulp.src('images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
-    );
+);
 
 gulp.task('css', function() {
     gulp.src([
-        './css/main.css',
-        './css/blog.css',
-        './css/contact.css',
+            './css/main.css',
+            './css/blog.css',
+            './css/contact.css',
         ])
-    .pipe(concat('styles.css'))
-    .pipe(cssMin())
-    .pipe(gulp.dest('./dist'));
+        .pipe(concat('styles.css'))
+        .pipe(cssMin())
+        .pipe(gulp.dest('./dist'));
 });
 gulp.task('scripts', function() {
     gulp.src([
-        './js/jquery-2.2.4.js',
-        './js/bootstrap.min.js',
-        './js/angular.js',
-        './js/angular-cookies.js',
-        './node_modules/sw-toolbox/sw-toolbox.js'
+            './js/jquery-2.2.4.js',
+            './js/bootstrap.min.js',
+            './js/angular.js',
+            './js/angular-cookies.js',
+            './node_modules/sw-toolbox/sw-toolbox.js'
         ])
-    .pipe(concat('libs.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+
+    gulp.src('./node_modules/sw-toolbox/sw-toolbox.js')
+        .pipe(concat('sw-toolbox.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
 });
--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-importScripts('dist/libs.js');
+importScripts('/dist/sw-toolbox.js');
 toolbox.options.debug = false;
 
 toolbox.precache([
     '/',
     '/dist/styles.css',
+    '/dist/images/avatar--contact.jpeg',
+    '/dist/images/gmail__icon.svg',
+    '/dist/images/phone__icon.svg',
+    '/dist/images/Octocat.png',
+    '/dist/images/instagram__icon.png',
+    '/dist/images/facebook__icon.svg',
+    '/dist/images/twitter__icon.svg',
+    '/dist/images/googleplus__icon.svg',
     '/js/app.js',
     '/pages/blog/',
     '/pages/contact/',
@@ -11,6 +19,14 @@ toolbox.precache([
 ]);
 toolbox.router.get('/', toolbox.fastest);
 toolbox.router.get('/dist/styles.css', toolbox.fastest);
+toolbox.router.get('/dist/images/avatar--contact.jpeg', toolbox.fastest);
+toolbox.router.get('/dist/images/gmail__icon.svg', toolbox.fastest);
+toolbox.router.get('/dist/images/phone__icon.svg', toolbox.fastest);
+toolbox.router.get('/dist/images/Octocat.png', toolbox.fastest);
+toolbox.router.get('/dist/images/instagram__icon.png', toolbox.fastest);
+toolbox.router.get('/dist/images/facebook__icon.svg', toolbox.fastest);
+toolbox.router.get('/dist/images/twitter__icon.svg', toolbox.fastest);
+toolbox.router.get('/dist/images/googleplus__icon.svg', toolbox.fastest);
 toolbox.router.get('/js/app.js', toolbox.fastest);
 toolbox.router.get('/pages/blog/', toolbox.fastest);
 toolbox.router.get('/pages/contact/', toolbox.fastest);
